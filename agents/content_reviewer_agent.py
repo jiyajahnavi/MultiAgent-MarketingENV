1205934a80c6bafd481c787ee47a53cd5279e824
goblinasaddy <jr.aditya004@gmail.com>
2026-04-02 10:57:15 +0530

    new branch created

--- agents/content_reviewer_agent.py
+++
@@ -1,5 +0,0 @@
-from agents.base_agent import BaseAgent
-
-class ContentReviewerAgent(BaseAgent):
-    def __init__(self):
-        super().__init__(name="ContentReviewerAgent", tool="review_content")
--- agents/social_media_strategist_agent.py
+++
@@ -1,5 +0,0 @@
-from agents.base_agent import BaseAgent
-
-class SocialMediaStrategistAgent(BaseAgent):
-    def __init__(self):
-        super().__init__(name="SocialMediaStrategistAgent", tool="add_hashtags")
